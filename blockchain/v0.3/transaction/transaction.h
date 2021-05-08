26bcd4e53a7061a76f7e02ff2ffefe7a23462302
kamalaaouad <kamalaaouad1999@gmail.com>
2021-05-08 16:16:29 +0000

    1. Create unspent transaction output

--- blockchain/v0.3/transaction/transaction.h
+++ blockchain/v0.3/transaction/transaction.h
@@ -32,6 +32,26 @@ typedef struct tx_out_s
 	uint8_t		hash[SHA256_DIGEST_LENGTH];
 } tx_out_t;
 
+/**
+ * struct unspent_tx_out_s - Unspent transaction output
+ *
+ * Description: This structure helps identify transaction outputs that were not
+ * used in any transaction input yet, making them "available".
+ *
+ * @block_hash: Hash of the Block containing the transaction @tx_id
+ * @tx_id:	ID of the transaction containing @out
+ * @out:	Copy of the referenced transaction output
+ */
+typedef struct unspent_tx_out_s
+{
+	uint8_t	block_hash[SHA256_DIGEST_LENGTH];
+	uint8_t	tx_id[SHA256_DIGEST_LENGTH];
+	tx_out_t	out;
+} unspent_tx_out_t;
+
 
 tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN]);
+unspent_tx_out_t *unspent_tx_out_create(
+	uint8_t block_hash[SHA256_DIGEST_LENGTH],
+	uint8_t tx_id[SHA256_DIGEST_LENGTH], tx_out_t const *out);
 #endif
