e0eb4ee81b1cb186c198206e6b6422d099f70ed3
kamalaaouad <61232328+kamalaaouad@users.noreply.github.com>
2021-05-09 10:36:58 +0000

    tx_out_create.c

--- blockchain/v0.3/transaction/tx_out_create.c
+++ blockchain/v0.3/transaction/tx_out_create.c
@@ -4,17 +4,22 @@
  * tx_out_create - creates a new transaction output structure
  * @amount: the transaction amount
  * @pub: public key of receiver
- * Return: pointer to new structure or NULL
+ *
+ * Return: pointer to the transaction outputon success, else NULL
  */
 tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN])
 {
-	tx_out_t *t = calloc(1, sizeof(*t));
+	tx_out_t *txOut = calloc(1, sizeof(tx_out_t));
 
-	if (!t)
+	if (!txOut)
 		return (NULL);
-	t->amount = amount;
-	memcpy(t->pub, pub, sizeof(t->pub));
-	if (!sha256((int8_t const *)t, sizeof(t->amount) + sizeof(t->pub), t->hash))
-		return (free(t), NULL);
-	return (t);
+	txOut->amount = amount;
+	memcpy(txOut->pub, pub, sizeof(txOut->pub));
+	if (!sha256((void *)txOut, sizeof(txOut->amount) + sizeof(txOut->pub),
+		    txOut->hash))
+	{
+		free(txOut);
+		return (NULL);
+	}
+	return (txOut);
 }
