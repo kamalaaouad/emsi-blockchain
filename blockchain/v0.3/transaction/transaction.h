36b32f05654319c7a53cee67d1af9759a6d745c0
kamalaaouad <kamalaaouad1999@gmail.com>
2021-05-09 11:57:52 +0000

    tasks 0 and 1

---
+++ blockchain/v0.3/transaction/transaction.h
@@ -0,0 +1,57 @@
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
+
+#include "blockchain.h"
+
+/**
+ * struct transaction_s - Transaction structure
+ *
+ * @id:      Transaction ID. A hash of all the inputs and outputs.
+ *           Prevents further alteration of the transaction.
+ * @inputs:  List of `tx_in_t *`. Transaction inputs
+ * @outputs: List of `tx_out_t *`. Transaction outputs
+ */
+typedef struct transaction_s
+{
+	uint8_t		id[SHA256_DIGEST_LENGTH];
+	llist_t		*inputs;
+	llist_t		*outputs;
+} transaction_t;
+
+/**
+ * struct tx_out_s - Transaction output
+ *
+ * @amount: Amount received
+ * @pub:	Receiver's public address
+ * @hash:	Hash of @amount and @pub. Serves as output ID
+ */
+typedef struct tx_out_s
+{
+	uint32_t	amount;
+	uint8_t		pub[EC_PUB_LEN];
+	uint8_t		hash[SHA256_DIGEST_LENGTH];
+} tx_out_t;
+
+/**
+ * struct unspent_tx_out_s - Unspent transaction output
+ *
+ * Description: This structure helps identify transaction outputs that were not
+ * used in any transaction input yet, making them "available".
+ *
+ * @block_hash: Hash of the Block containing the transaction @tx_id
+ * @tx_id:	ID of the transaction containing @out
+ * @out:	Copy of the referenced transaction output
+ */
+typedef struct unspent_tx_out_s
+{
+	uint8_t	block_hash[SHA256_DIGEST_LENGTH];
+	uint8_t	tx_id[SHA256_DIGEST_LENGTH];
+	tx_out_t	out;
+} unspent_tx_out_t;
+
+
+tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN]);
+unspent_tx_out_t *unspent_tx_out_create(
+	uint8_t block_hash[SHA256_DIGEST_LENGTH],
+	uint8_t tx_id[SHA256_DIGEST_LENGTH], tx_out_t const *out);
+#endif
