ffca8be381228127574d9b50b3e4149683c1123b
kamalaaouad <kamalaaouad1999@gmail.com>
2021-05-09 10:34:01 +0000

    transactions

--- blockchain/v0.3/blockchain.h
+++ blockchain/v0.3/blockchain.h
@@ -1,14 +1,11 @@
 #ifndef BLOCKCHAIN_H
 #define BLOCKCHAIN_H
 
-#include "../../crypto/hblk_crypto.h"
-#include "provided/endianness.h"
-
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <../../llist/llist.h>
+#include <llist.h>
 #include <time.h>
 
 #include <sys/types.h>
@@ -18,21 +15,30 @@
 
 #include <openssl/sha.h>
 
+#include "../../crypto/hblk_crypto.h"
+#include "provided/endianness.h"
+#include "transaction/transaction.h"
+
+
+
 #define MIN(x, y) ((x) < (y) ? (x) : (y))
 
 #define HBLK_MAGIC "HBLK"
-#define HBLK_VERSION "0.1"
+#define HBLK_VERSION "0.3"
+
+#define COINBASE_AMOUNT 50
 
 /**
  * struct blockchain_s - Blockchain structure
  *
- * @chain: Linked list of pointers to block_t
+ * @chain:	Linked list of Blocks
+ * @unspent: Linked list of unspent transaction outputs
  */
 typedef struct blockchain_s
 {
-	llist_t	 *chain;
+	llist_t	*chain;
+	llist_t	*unspent;
 } blockchain_t;
-
 
 /**
  * struct block_info_s - Block info structure
@@ -60,6 +66,8 @@ typedef struct block_info_s
 } block_info_t;
 
 #define BLOCKCHAIN_DATA_MAX 1024
+#define BLOCK_GENERATION_INTERVAL 1
+#define DIFFICULTY_ADJUSTMENT_INTERVAL 5
 
 /**
  * struct block_data_s - Block data
@@ -82,12 +90,14 @@ typedef struct block_data_s
  *
  * @info: Block info
  * @data: Block data
+ * @transactions: linked list of transaction ids
  * @hash: 256-bit digest of the Block, to ensure authenticity
  */
 typedef struct block_s
 {
 	block_info_t	info; /* This must stay first */
 	block_data_t	data; /* This must stay second */
+	llist_t *transactions;
 	uint8_t	 hash[SHA256_DIGEST_LENGTH];
 } block_t;
 
@@ -104,18 +114,26 @@ typedef struct block_s
 		"Holberton School", /* buffer */ \
 		16 /* len */ \
 	}, \
+	NULL, /* transactions */ \
 	"\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d" \
 	"\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03" \
 }
 
 blockchain_t *blockchain_create(void);
-block_t *block_create(block_t const *prev, int8_t const *data, uint32_t data_len);
+block_t *block_create(block_t const *prev, int8_t const *data,
+	uint32_t data_len);
 void block_destroy(block_t *block);
 void blockchain_destroy(blockchain_t *blockchain);
-uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH]);
+uint8_t *block_hash(block_t const *block,
+	uint8_t hash_buf[SHA256_DIGEST_LENGTH]);
 int blockchain_serialize(blockchain_t const *blockchain, char const *path);
 blockchain_t *blockchain_deserialize(char const *path);
-int block_is_valid(block_t const *block, block_t const *prev_block);
-int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH], uint32_t difficulty);
+llist_t *deserialize_blocks(int fd, uint32_t size, uint8_t endianness);
+int block_is_valid(block_t const *block, block_t const *prev_block,
+	llist_t *all_unspent);
+int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
+	uint32_t difficulty);
 void block_mine(block_t *block);
+uint32_t blockchain_difficulty(blockchain_t const *blockchain);
+
 #endif
--- blockchain/v0.3/test/blockchain_create-main.c
+++ blockchain/v0.3/test/blockchain_create-main.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <../../llist/llist.h>
+#include <llist.h>
 
 #include "blockchain.h"
 
